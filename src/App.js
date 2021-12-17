350964d96af2a3115cae4d1cd4b76121420a61ad
RayanElChakik <rayan.elchakik@gmail.com>
2021-12-17 09:28:49 +0000

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,29 +1,71 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
-
-import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
 
 import "./App.css";
+import clear from "./img/weather-icons/clear.svg";
+import partlycloudy from "./img/weather-icons/partlycloudy.svg";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "Rayan"
-    };
-  }
-
-  handleInputChange = value => {
-    this.setState({ name: value });
-  };
 
   render() {
     return (
       <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+          <header>
+          <div className="header--container">
+              <input type="text" className="header--search" placeholder="Type in a city name"/>
+              <button className="header--button"> Find Weather</button>
+          </div>
+          </header>
+          <div className="main--container"> 
+              <div className="main-weather">
+              <div className="main-temperature-image">
+                      <img className="main__image-temp" src="https://via.placeholder.com/350x150" alt="Cloud-weather-image" placeholder="clear-clouds"/>
+                      <p>Overcast Clouds</p>
+              </div>
+              <div className="main-temperature-information">
+                  <p><span>Temperature </span>  <span class="span--delete"> 10&deg; to 11 &#8451;</span></p>
+                  <div className="main-temperature-addition">
+                    <p> <span>Humidity</span>  78% <span class="indent">Pressure </span> 1008.48</p>  
+              </div>
+              </div>
+              </div>
+              <div className="grid__container">
+                  <div className="grid--mobile grid--temp--one">
+                      <time>3:00</time>
+                      <img className="image-placeholder" src="https://via.placeholder.com/140x100" alt="Partly-cloudy Weather" placeholder="Partly-cloudy weather"/>
+                      <p>26 &deg;</p>
+                  </div>
+                  <div className="grid--mobile grid--temp--two">
+                  <time>6:00</time>
+                    <img className="image-placeholder" src="https://via.placeholder.com/140x100" alt="Partly-cloudy Weather" placeholder="Partly-cloudy weather"/>
+                    <p>26 &deg;</p>
+                  </div>
+                  <div className="grid--mobile grid--temp--three">
+                  <time>9:00</time>
+                    <img className="image-placeholder" src="https://via.placeholder.com/140x100" alt="Clear Weather" placeholder="Clear weather"/>
+                    <p>28 &deg;</p>
+                  </div>
+                  <div className="grid--mobile grid--temp--four">
+                  <time>12:00</time>
+                    <img className="image-placeholder" src="https://via.placeholder.com/140x100" alt="Clear Weather" placeholder="Clear weather"/>
+                    <p>28 &deg;</p>
+                  </div>
+                  <div className="grid--mobile grid--temp--five">
+                  <time>15:00</time>
+                    <img className="image-placeholder" src="https://via.placeholder.com/140x100" alt="Clear Weather" placeholder="Clear weather"/>
+                    <p>28 &deg;</p>
+                  </div>
+                  <div className="grid--mobile grid--temp--six">
+                  <time>18:00</time>
+                    <img className="image-placeholder" src="https://via.placeholder.com/140x100" alt="Clear Weather" placeholder="Clear weather"/>
+                    <p>28 &deg;</p>
+                  </div>
+                  <div className="grid--mobile grid--temp--seven">
+                  <time>21:00</time>
+                    <img className="image-placeholder" src="https://via.placeholder.com/140x100" alt="Clear Weather" placeholder="Clear weather"/>
+                    <p>26 &deg;</p>
+                  </div>
+              </div>
+      </div>
       </div>
     );
   }
