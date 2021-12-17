fec4aea2466e987f88164c7be12c5c7bd9f56d36
RayanElChakik <rayan.elchakik@gmail.com>
2021-12-17 20:13:27 +0000

    Weather API: Extracting the values from the Fake Weather json

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import React, { Component } from "react";
 import "./App.css";
 import Search from "./components/Search";
 import WeatherItem, { MainWeather } from "./components/WeatherItem";
+import fakeWeather from "./fakeWeatherData.json";
 import clear from "./img/weather-icons/clear.svg";
 import partlycloudy from "./img/weather-icons/partlycloudy.svg";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      weatherList: fakeWeather.list,
+    };
+  }
+  
+  handleInputChange = value => {
+    this.setState({ name: value });
+  };
 
   render() {
     return (
@@ -15,15 +27,14 @@ class App extends Component {
           <Search />
           </header>
           <div className="main--container"> 
-              <MainWeather />
+              <MainWeather humidity={this.state.weatherList[0].main.humidity} pressure={this.state.weatherList[0].main.pressure} tempMin={this.state.weatherList[0].main.temp_min} tempMax={this.state.weatherList[0].main.temp_max}
+              discriptions={this.state.weatherList[0].weather[0].description}/>
               <div className="upcoming-weather">
-                  < WeatherItem />
-                  < WeatherItem />
-                  <WeatherItem />
-                  <WeatherItem />
-                  <WeatherItem />
-                  <WeatherItem />
-                  <WeatherItem />
+                {this.state.weatherList.map((item,index) =>{
+                  if(index > 0 && index <8){
+                    return  < WeatherItem time={item.dt_txt} dayDegree={item.main.temp} />
+                  }
+                })}
               </div>
       </div>
       </div>
--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -6,10 +6,10 @@ import clear from "../img/weather-icons/clear.svg";
 export default class WeatherItem extends Component {
   render() {
     return (
-      <div class="flex--border">
-      <time>3:00</time>
+      <div className="flex--border">
+      <time>{this.props.time.split(" ")[1].substring(0,5)}</time>
       <img className="image-placeholder" src={partlycloudy} alt="Partly-cloudy Weather" placeholder="Partly-cloudy weather"/>
-      <p>26 &deg;</p>
+      <p>{Math.round(this.props.dayDegree-273.15)}&deg;</p>
   </div>
       );
   }
@@ -20,13 +20,13 @@ export class MainWeather extends Component {
       <div className="main-weather">
               <div className="main-temperature-image">
                       <img className="main__image-temp"src={clear} alt="Cloud-weather-image" placeholder="clear-clouds"/>
-                      <p>Overcast Clouds</p>
+                      <p>{this.props.discriptions}</p>
               </div>
               <div className="main-temperature-information">
-                  <p><span>Temperature </span>  <span class="span--delete"> 10&deg; to 11 &#8451;</span></p>
+                  <p><span>Temperature </span>  <span className="span--delete"> {Math.round(this.props.tempMin-273.15)}&deg; to {Math.round(this.props.tempMax-273.15)} &#8451;</span></p>
                   <div className="main-temperature-addition">
-                    <p> <span>Humidity</span>  78% </p>
-                    <p> <span class="indent">Pressure </span> 1008.48</p>  
+                    <p> <span>Humidity</span>  {this.props.humidity}% </p>
+                    <p> <span className="indent">Pressure </span> {this.props.pressure}</p>  
               </div>
               </div>
               </div>
