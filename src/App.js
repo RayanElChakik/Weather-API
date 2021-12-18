bbb03fd20b60cbb4fb74417943a0505dde14dfc9
RayanElChakik <rayan.elchakik@gmail.com>
2021-12-18 21:32:22 +0000

    Weather API: Getting the LIVE weather data-Matching Ids

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import fakeWeather from "./fakeWeatherData.json";
 import clear from "./img/weather-icons/clear.svg";
 import partlycloudy from "./img/weather-icons/partlycloudy.svg";
 
+const API_Key = "bd89960110bd1c361fcd7effa2b2904e"
+
 class App extends Component {
 
   constructor(props) {
@@ -17,7 +19,7 @@ class App extends Component {
     };
   }
   getWeatherList= (country =>{
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${country}&cnt=8&units=metric&appid=bd89960110bd1c361fcd7effa2b2904e`)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${country}&cnt=8&units=metric&appid=${API_Key}`)
     .then(res=> res.json())
     .then(data=>this.setState({weatherList:data.list}))
   })
@@ -25,7 +27,6 @@ class App extends Component {
     this.setState({ country: value });
     this.getWeatherList(value)
   };
-
   render() {
     if(this.state.country !="" && this.state.weatherList !== undefined){
       return (
@@ -35,15 +36,18 @@ class App extends Component {
             </header>
             <div className="main--container"> 
                 <MainWeather 
-                humidity={this.state.weatherList[0].main.humidity} 
-                pressure={this.state.weatherList[0].main.pressure} 
-                tempMin={this.state.weatherList[0].main.temp_min} 
-                tempMax={this.state.weatherList[0].main.temp_max}
-                discriptions={this.state.weatherList[0].weather[0].description}/>
+                currentWeather = {this.state.weatherList[0]}
+                // humidity={this.state.weatherList[0].main.humidity} 
+                // pressure={this.state.weatherList[0].main.pressure} 
+                // tempMin={this.state.weatherList[0].main.temp_min} 
+                // tempMax={this.state.weatherList[0].main.temp_max}
+                // discriptions={this.state.weatherList[0].weather[0].description}
+                iconId = {this.state.weatherList[0].weather[0].id}/>
                 <div className="upcoming-weather">
                   {this.state.weatherList.map((item,index) =>{
                     if(index > 0 && index <8){
-                      return  < WeatherItem time={item.dt_txt} dayDegree={item.main.temp} />
+                      return  < WeatherItem time={item.dt_txt} dayDegree={item.main.temp} 
+                      iconId={item.weather[0].id}/>
                     }
                   })}
                 </div>
--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -3,30 +3,69 @@ import { render } from "react-dom";
 import partlycloudy from "../img/weather-icons/partlycloudy.svg";
 import clear from "../img/weather-icons/clear.svg";
 
+
 export default class WeatherItem extends Component {
+  get_WeatherIcons(idRange){
+    if( idRange < 300 ){
+       return "storm";
+    } else if (idRange >= 300 && idRange <= 499){
+      return "drizzle";
+    } else if (idRange >= 500 && idRange <= 599){
+      return "rain";
+    } else if(idRange >= 600 && idRange <= 699){
+     return "snow";  
+    } else if (idRange >= 700 && idRange <= 799){
+      return "fog";
+    } else if (idRange === 800){
+      return "clear";
+    } else if (idRange === 801){
+      return "partlycloudy";
+    } else if (idRange > 801 && idRange <= 805){
+      return "mostlycloudy";
+    }
+    }
   render() {
     return (
       <div className="flex--border">
       <time>{this.props.time.split(" ")[1].substring(0,5)}</time>
-      <img className="image-placeholder" src={partlycloudy} alt="Partly-cloudy Weather" placeholder="Partly-cloudy weather"/>
+      <img className="image-placeholder" src={require(`../img/weather-icons/${this.get_WeatherIcons(this.props.iconId)}.svg`)} alt="Partly-cloudy Weather" placeholder="Partly-cloudy weather"/>
       <p>{Math.round(this.props.dayDegree)}&deg;</p>
   </div>
       );
   }
 }
 export class MainWeather extends Component {
+  get_WeatherIcons(idRange){
+    if( idRange < 300 ){
+       return "storm";
+    } else if (idRange >= 300 && idRange <= 499){
+      return "drizzle";
+    } else if (idRange >= 500 && idRange <= 599){
+      return "rain";
+    } else if(idRange >= 600 && idRange <= 699){
+     return "snow";  
+    } else if (idRange >= 700 && idRange <= 799){
+      return "fog";
+    } else if (idRange === 800){
+      return "clear";
+    } else if (idRange === 801){
+      return "partlycloudy";
+    } else if (idRange > 801 && idRange <= 805){
+      return "mostlycloudy";
+    }
+    }
   render() {
     return (
       <div className="main-weather">
               <div className="main-temperature-image">
-                      <img className="main__image-temp"src={clear} alt="Cloud-weather-image" placeholder="clear-clouds"/>
-                      <p>{this.props.discriptions}</p>
+                      <img className="main__image-temp" src={require(`../img/weather-icons/${this.get_WeatherIcons(this.props.iconId)}.svg`)} alt="Cloud-weather-image" placeholder="clear-clouds"/>
+                      <p>{this.props.currentWeather.weather[0].description}</p>
               </div>
               <div className="main-temperature-information">
-                  <p><span>Temperature </span>  <span className="span--delete"> {Math.round(this.props.tempMin)}&deg; to {Math.round(this.props.tempMax)} &#8451;</span></p>
+                  <p><span>Temperature </span>  <span className="span--delete"> {Math.round(this.props.currentWeather.main.temp_min)}&deg; to {Math.round(this.props.currentWeather.main.temp_max)} &#8451;</span></p>
                   <div className="main-temperature-addition">
-                    <p> <span>Humidity</span>  {this.props.humidity}% </p>
-                    <p> <span className="indent">Pressure </span> {this.props.pressure}</p>  
+                    <p> <span>Humidity</span>  {this.props.currentWeather.main.humidity}% </p>
+                    <p> <span className="indent">Pressure </span> {this.props.currentWeather.main.pressure}</p>  
               </div>
               </div>
               </div>
